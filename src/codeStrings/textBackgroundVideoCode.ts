c24207189cbc3df02bebfb0c5f88152d715f15f4
kazutakanakamura711 <a711st777@gmail.com>
2024-04-14 23:30:38 +0900

    Fix

--- src/codeStrings/textBackgroundVideoCode.ts
+++ src/codeStrings/textBackgroundVideoCode.ts
@@ -1,53 +1,52 @@
 export const textBackgroundVideoCode = `
-  const text = 'SampleText';
-  export const TextBackgroundVideo = () => {
-    return (
+import { text, videoUrl } from '@/shared/utils/sampleConst';
+import { Box, Text } from '@chakra-ui/react';
+
+export const TextBackgroundVideo = () => {
+  return (
+    <Box
+      position="absolute"
+      w="100%"
+      h="calc(100vh - 72px)"
+      display="grid"
+      placeItems="center"
+      overflow="hidden"
+    >
       <Box
+        as="video"
         position="absolute"
+        top="0"
+        left="0"
         w="100%"
-        h="100vh"
-        display="grid"
-        placeItems="center"
-        overflow="hidden"
+        h="100%"
+        objectFit="cover"
+        controls
+        playsInline
+        autoPlay
+        muted
+        loop
       >
-        <Box
-          as="video"
-          position="absolute"
-          top="0"
-          left="0"
-          w="100%"
-          h="100%"
-          objectFit="cover"
-          controls
-          playsInline
-          autoPlay
-          muted
-          loop
-        >
-          <source
-            src="https://www.w3schools.com/howto/rain.mp4"
-            type="video/mp4"
-          />
-        </Box>
-        <Text
-          as="h1"
-          position="absolute"
-          top="0"
-          left="0"
-          w="100%"
-          h="full"
-          bg="white"
-          fontSize="16vw"
-          textAlign="center"
-          lineHeight="100vh"
-          fontFamily="'Impact'"
-          mixBlendMode="screen"
-          letterSpacing="1.1vw"
-          color="black"
-        >
-          {text}
-        </Text>
+        <source src={videoUrl} type="video/mp4" />
       </Box>
-    )
-  }
+      <Text
+        as="h1"
+        position="absolute"
+        top="0"
+        left="0"
+        w="100%"
+        h="full"
+        bg="white"
+        fontSize="16vw"
+        textAlign="center"
+        lineHeight="100vh"
+        fontFamily="'Impact'"
+        mixBlendMode="screen"
+        letterSpacing="1.1vw"
+        color="black"
+      >
+        {text}
+      </Text>
+    </Box>
+  );
+};
 `;
